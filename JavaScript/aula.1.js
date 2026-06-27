9e73169bff64db09d1b1fa2970bd6d3e7be1095e
WesleyMarlon <wesleymarlon0827@gmail.com>
2026-06-27 16:47:00 -0300

    Update aula.1.js

--- JavaScript/aula.1.js
+++ JavaScript/aula.1.js
@@ -1,2 +1,14 @@
-let nome = "Wesley"
+const nome = "Python linguagem foda" // Testando
 console.log(nome)
+console.log(2 + 2)
+let n1 = 3
+let n2 = 5
+const idade = 22
+let soma = n1 + n2
+console.log(`Se o primeiro número é ${n1} e o segundo é ${n2}. Então a soma de ambos é igual a ${soma}`)
+
+let cal = "menor de idade"
+if (idade >= 18) {
+    cal = "maior de idade"
+}
+console.log(`Eu tenho ${idade} anos. Então eu sou ${cal}`)
