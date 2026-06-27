0dbac4a643243c90d0ce07ef37341d13fded620d
WesleyMarlon <wesleymarlon0827@gmail.com>
2026-06-27 12:35:17 -0300

    Update aula.1.js

--- JavaScript/aula.1.js
+++ JavaScript/aula.1.js
@@ -1,4 +1,2 @@
-var n1 = Number(window.prompt('Escolha um número: '))
-var n2 = Number(window.prompt('Outro número: '))
-var soma = n1 + n2
-window.alert(`A soma dos números resulta em: ${soma}`)
+let nome = "Wesley"
+console.log(nome)
