fbc0740b40d57a3030286b6cd7fbc93b6c480b9f
WesleyMarlon <wesleymarlon0827@gmail.com>
2026-09-18 17:48:12 -0300

    Update aula.js

--- JavaScript.2/aula.js
+++ JavaScript.2/aula.js
@@ -1,4 +1,9 @@
-var idade = 22
-if ( idade < 18 || > 67 ) {
+var idade = 15
+
+if ( idade < 16 ) {
+    console.log('Voto nulo')
+} else if ( idade < 18 || idade > 65 ) {
     console.log('Voto opcional')
-} else if
+} else {
+    console.log('Voto obrigatório')
+}
